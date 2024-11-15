85f8100a4bb52d4572812038ddcd25398092e14d
Sarthak Sahu <14sarthak.sahu@gmail.com>
2024-11-15 18:43:16 +0530

    project-three

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Highlights from './components/Highlights';
+import Model from './components/Model';
+import Features from './components/Features';
+import HowItWorks from './components/HowItWorks';
+import Footer from './components/Footer';
+import LocomotiveScroll from 'locomotive-scroll';
+
+
+
+
+import * as Sentry from '@sentry/react';
+
+const App = () => {
+  const locomotiveScroll = new LocomotiveScroll();
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
+      <Features />
+      <HowItWorks />
+      <Footer />
+    </main>
+  )
+}
+
+export default Sentry.withProfiler(App);
